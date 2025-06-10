6ece06b9bd1428808bcffd81ee725e380d26535a
Suhail <suhail.shaik@everest.engineering>
2025-06-10 15:15:18 +0530

    fix: Resolve rebase conflicts in multiplication branch

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-
-
 function add(a,b){
     return a+b;
 }
 
 function divideByTwo(value){
     return value/2;
-<<<<<<< HEAD
 
 }
-=======
-}
-
 function multiply(v,h){
     return v*h;
 }
->>>>>>> feat: Add multiply function to multiply 2 numbers
